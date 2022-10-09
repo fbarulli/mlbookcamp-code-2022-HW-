8b0ddc25c2a6842e3e4dc3df31ef22fb8a0c1570
fabian <fbarulli@gmail.com>
2022-10-09 13:42:55 +0200

    Add files via upload

---
+++ hw5/q4_test.py
@@ -0,0 +1,9 @@
+import requests
+
+url = "https://localhost:9696/predict"
+
+client = {"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
+respose = requests.post(url, json=client).json()
+
+
+print(respose)
